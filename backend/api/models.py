646a66d0253796d2430de279f57150831bdf310b
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-11-30 17:22:13 +0330

    Made the task model

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,14 @@
+from datetime  import date
 from django.db import models
+from django.contrib.auth.models import User 
 
 # Create your models here.
+
+class TaskModel(models.Model): 
+    name = models.CharField(max_length=128, blank=False) 
+    summary = models.TextField() 
+    start_date = models.DateField(default=date.today) 
+    deadline = models.DateField()
+
+    def __str__(self):
+        return self.name
