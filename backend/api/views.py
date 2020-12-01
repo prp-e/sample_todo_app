951554163b245b9ea94e010f5c9e12bb9e939ce3
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-01 12:36:06 +0330

    Views are made

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets, permissions 
+from .models import TaskModel 
+from django.contrib.auth.models import User 
+from .serializers import UserSerializer, TaskSerializer
 # Create your views here.
+
+class TaskView(viewsets.ModelViewSet): 
+    queryset = TaskModel.objects.filter(user_id = request.user.id )
+    serializer_class = TaskSerializer 
+    permission_classes = [permissions.IsAuthenticated] 
+
+class UserView(viewsets.ModelViewSet): 
+    queryset = User.objects.all() 
+    serializer_class = UserSerializer 
+    permission_classes = [permissions.IsAdminUser]
