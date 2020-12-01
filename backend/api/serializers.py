d91321785905c2fca51939f3508927eefcb47da9
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-01 12:27:28 +0330

    Serialization is done

---
+++ backend/api/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers 
+from django.contrib.auth.models import User 
+from .models import TaskModel 
+
+class TaskSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = TaskModel 
+        fields = "__all__" 
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = User 
+        fields = (
+            "id", 
+            "username", 
+            "is_admin"
+        )
