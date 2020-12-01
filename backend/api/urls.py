91509d311bae19a66ca50d7ffd2b43d43db48ac4
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-01 14:27:06 +0330

    Routing is OK

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter() 
-router.register('tasks/', views.TaskView) 
-router.register('users/', views.UserView)
+router.register('tasks', views.TaskView) 
+router.register('users', views.UserView)
 
 urlpatterns = [
     path('v1/', include(router.urls))
