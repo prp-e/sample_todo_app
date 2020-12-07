632be7a6ffdfeb77c2f8c68535282c9cc5a52b40
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-07 17:52:36 +0330

    Permissions better now

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 class IsAssigned(permissions.BasePermission):
     """ Determines if the damn thing is from the user or not """ 
     def has_object_permission(self, request, view, obj):
-        if obj.assigned_to == request.user: 
+        if obj.user == request.user: 
             return True 
         else: 
             return False
