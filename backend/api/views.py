2300746a39ad2ab56b6c218970e8f8f6e5185ac9
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-16 17:24:06 +0330

    Now it is how it should be

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework import viewsets, permissions 
+from rest_framework import viewsets, permissions
 from .permissions import IsAssigned
 from .models import TaskModel 
 from django.contrib.auth.models import User 
@@ -7,6 +7,9 @@ from .serializers import UserSerializer, TaskSerializer
 # Create your views here.
 
 class TaskView(viewsets.ModelViewSet):  
+    def get_queryset(self):
+        return super().get_queryset().filter(user=self.request.user)
+    
     queryset = TaskModel.objects.all()
     serializer_class = TaskSerializer 
     permission_classes = [IsAssigned] 
