1936c3703b311372a27eefa023b5d0a8bd86c640
Muhammadreza Haghiri <haghiri75@gmail.com>
2020-12-17 16:56:48 +0330

    Got the form

--- TodoApp/lib/main.dart
+++ TodoApp/lib/main.dart
@@ -13,9 +13,31 @@ class TodoApp extends StatelessWidget {
           appBar: AppBar(
             title: Text('Sample Todo App'),
           ),
-          body: Center(
-            child: Text("Hello, World!"),
-          ),
+          body: LoginState(),
         ));
+  }
+}
+
+class LoginState extends StatefulWidget {
+  _LoginPage createState() => _LoginPage();
+}
+
+class _LoginPage extends State<LoginState> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text("Username: "),
+            TextField(),
+            Text("Password: "),
+            TextField(),
+            FlatButton(onPressed: null, child: Text("Login"))
+          ],
+        ),
+      ),
+    );
   }
 }
